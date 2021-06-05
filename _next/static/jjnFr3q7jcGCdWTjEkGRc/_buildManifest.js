cb042fb79cfb120249d45a506183296b038fa1b6
yagipy <yagipy@users.noreply.github.com>
2021-06-05 19:53:47 +0000

    deploy: d3a692fda811b8867e24bd017c9e05e7cb5cd5e5

---
+++ _next/static/jjnFr3q7jcGCdWTjEkGRc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-a9ee41d67f7c64b17711.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b4d26601432811bf2a86.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
